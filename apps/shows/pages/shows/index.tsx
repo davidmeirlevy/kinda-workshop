018b919bd6984b9348e5bd649a458155dc1644df
Avner Hattab <avner.h@nanox.vision>
2023-03-22 16:45:12 +0200

    show page wip

---
+++ apps/shows/pages/shows/index.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Shows = () => {
+  const [shows, setShows] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+};
+
+export const showsPage =  () => {
+    return (
+        <div>
+        </div>
+    )
+}
